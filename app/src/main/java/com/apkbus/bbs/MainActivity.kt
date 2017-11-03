2191672be75e524709c6908faf977ea06066c04f
nilin <11620967@qq.com>
2017-11-03 10:30:28 +0800

    Updata BBSSDK to 2.1.0 and add Moblink.

--- app/src/main/java/com/apkbus/bbs/MainActivity.kt
+++ app/src/main/java/com/apkbus/bbs/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.apkbus.bbs
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
 import com.mob.bbssdk.gui.BaseMainActivity
 import com.mob.bbssdk.model.ForumThreadAttachment
+import com.mob.moblink.RestoreSceneListener;
+import java.util.HashMap
+import com.mob.bbssdk.gui.BBSViewBuilder
+import com.mob.bbssdk.utils.StringUtils
+import com.mob.moblink.MobLink
 
-class MainActivity : BaseMainActivity() {
+
+class MainActivity : BaseMainActivity(), RestoreSceneListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MobLink.setRestoreSceneListener(this);
+    }
+
+    var strUrl: String? = null
+
+    override fun onReturnSceneIntent(p0: String?, p1: Intent?): Boolean {
+        return false
+    }
+
+    override fun onFinishCheckScene() {
+    }
+
+    override fun onBeginCheckScene() {
+    }
+
+    override fun onReturnSceneData(activity: Activity?, hashMap: HashMap<String, Any>?) {
+        val urlvalue = hashMap!!.get("ulurl")
+        if (urlvalue != null && urlvalue is String) {
+            strUrl = urlvalue as String
+
+            //open web link page if valid
+            if (!StringUtils.isEmpty(strUrl)) {
+                val web = BBSViewBuilder.getInstance().buildPageWeb()
+                web.setLink(strUrl)
+                web.show(this@MainActivity)
+            }
+        }
+    }
 
     override fun OnAttachmentClick(attachment: ForumThreadAttachment) {
-
         super.OnAttachmentClick(attachment)
-
     }
 }
+
+
