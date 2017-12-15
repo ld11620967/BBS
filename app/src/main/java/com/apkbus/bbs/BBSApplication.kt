ff83c48efeaa270257c8ff12adfe9ce040236c00
nilin <11620967@qq.com>
2017-12-15 18:54:20 +0800

    Updata to 2.2.0

--- app/src/main/java/com/apkbus/bbs/BBSApplication.kt
+++ app/src/main/java/com/apkbus/bbs/BBSApplication.kt
@@ -1,15 +1,11 @@
 package com.apkbus.bbs
 
 import android.app.Application
-import com.mob.MobSDK
-import com.mob.bbssdk.theme1.BBSTheme1
 
 
 class BBSApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MobSDK.init(this, "", "")
-        BBSTheme1.init()
     }
 }
--- app/src/main/java/com/apkbus/bbs/MainActivity.kt
+++ app/src/main/java/com/apkbus/bbs/MainActivity.kt
@@ -1,51 +1,16 @@
 package com.apkbus.bbs
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import com.mob.bbssdk.gui.BaseMainActivity
-import com.mob.bbssdk.model.ForumThreadAttachment
-import com.mob.moblink.RestoreSceneListener
-import java.util.HashMap
-import com.mob.bbssdk.gui.BBSViewBuilder
-import com.mob.bbssdk.utils.StringUtils
-import com.mob.moblink.MobLink
+import com.mob.bbssdk.theme1.BBSTheme1
 
 
 class MainActivity : BaseMainActivity() {
-    var strUrl: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        BBSTheme1.init()
         super.onCreate(savedInstanceState)
 
-        MobLink.setRestoreSceneListener(object : RestoreSceneListener {
-            override fun onBeginCheckScene() {
-
-            }
-
-            override fun onFinishCheckScene() {
-
-            }
-
-            override fun onReturnSceneIntent(s: String, intent: Intent): Boolean {
-                return false
-            }
-
-            override fun onReturnSceneData(activity: Activity, hashMap: HashMap<String, Any>) {
-                val urlvalue = hashMap["ulurl"]
-                if (urlvalue != null && urlvalue is String) {
-                    strUrl = urlvalue
-                }
-                //open web link page if valid
-                if (!StringUtils.isEmpty(strUrl)) {
-                    val web = BBSViewBuilder.getInstance().buildPageWeb()
-                    web.setLink(strUrl)
-                    web.show(this@MainActivity)
-                }
-            }
-        })
     }
 
-    override fun OnAttachmentClick(attachment: ForumThreadAttachment) {
-        super.OnAttachmentClick(attachment)
-    }
 }
